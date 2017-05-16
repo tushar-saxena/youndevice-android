e02b025b8f1597b4ebcae3912403495140d342f5
Anuj Garg <anujgargcse@gmail.com>
2017-05-16 17:45:56 +0530

    Firebase auth (#2)
    * firebase auth and user profile
    
    * mend
    
    firebase auth and user profile

--- app/src/main/java/com/youndevice/android/youndevice/activity/LoginActivity.java
+++ app/src/main/java/com/youndevice/android/youndevice/activity/LoginActivity.java
@@ -1,117 +1,98 @@
 package com.youndevice.android.youndevice.activity;
 
-import static com.youndevice.android.youndevice.YoundeviceApplication.retrofit;
+import java.util.Arrays;
 
-import com.youndevice.android.youndevice.BuildConfig;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GetTokenResult;
 import com.youndevice.android.youndevice.R;
-import com.youndevice.android.youndevice.backend.Model.AuthResponse;
-import com.youndevice.android.youndevice.backend.Model.Credential;
-import com.youndevice.android.youndevice.backend.Model.User;
-import com.youndevice.android.youndevice.service.LoginService;
+import com.youndevice.android.youndevice.YoundeviceApplication;
 import com.youndevice.android.youndevice.util.Preferences;
 
-import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.StringRes;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.widget.EditText;
-import butterknife.BindView;
+import android.view.View;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 123;
 
-    @BindView(R.id.toolbar)
-    Toolbar mToolbar;
-
-    @BindView(R.id.username)
-    EditText mUsername;
-
-    @BindView(R.id.password)
-    EditText mPassword;
-
-    LoginActivity activity;
-
-    private ProgressDialog authIndicator;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // -- Bind view
         ButterKnife.bind(this);
 
-        activity = this;
+        mAuth = FirebaseAuth.getInstance();
+    }
 
-        setSupportActionBar(mToolbar);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
 
-        if (BuildConfig.DEBUG) {
-            mUsername.setText("tushar.saxena@tothenew.com");
-            mPassword.setText("tushar");
+            // Successfully signed in
+            if (resultCode == ResultCodes.OK) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+
+                Preferences.of(getApplicationContext()).profile().set(YoundeviceApplication.User.profile);
+                Preferences.of(getApplicationContext()).name().set(YoundeviceApplication.User.name);
+                Preferences.of(getApplicationContext()).email().set(YoundeviceApplication.User.email);
+                return;
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    showSnackbar(R.string.sign_in_cancelled);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackbar(R.string.no_internet_connection);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackbar(R.string.unknown_error);
+                    return;
+                }
+            }
+
+            showSnackbar(R.string.unknown_sign_in_response);
         }
     }
 
-
-    @OnClick(R.id.button_login)
-    public void login() {
-
-        Credential credential = new Credential();
-        credential.setEmailId(mUsername.getText().toString());
-        credential.setPassword(mPassword.getText().toString());
-        LoginService service = retrofit.create(LoginService.class);
-
-        Call call = service.login(credential);
-
-        call.enqueue(new Callback<AuthResponse>() {
-                         @Override
-                         public void onResponse(Call<AuthResponse> call, Response<AuthResponse> response) {
-                             if (authIndicator.isShowing()){
-                                 authIndicator.dismiss();
-                             }
-                             if (response.isSuccessful()) {
-                                 User user = response.body().getData();
-                                 Preferences.of(getApplicationContext()).firstName().set(user.getFirstName());
-                                 Preferences.of(getApplicationContext()).lastName().set(user.getLastName());
-                                 Preferences.of(getApplicationContext()).token().set(user.getToken());
-                                 Preferences.of(getApplicationContext()).authenticated().set(true);
-                                 Preferences.of(getApplicationContext()).email().set(mUsername.getText().toString());
-
-                                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                                 activity.finish();
-
-                                 Log.d("Success", "Success");
-                             } else {
-                                 activity.showError(R.string.error_login_credential);
-                             }
-                         }
-
-                         @Override
-                         public void onFailure(Call<AuthResponse> call, Throwable t) {
-                             if (authIndicator.isShowing()){
-                                 authIndicator.dismiss();
-                             }
-                             activity.showError(R.string.error_login_internet);
-                         }
-                     });
-
-        authIndicator=new ProgressDialog(this);
-        authIndicator.setCancelable(false);
-        authIndicator.setMessage("logging in...");
-        authIndicator.show();
-
+    private void showSnackbar(int id) {
+        View parentLayout = findViewById(R.id.root_view);
+        Snackbar.make(parentLayout, id, Snackbar.LENGTH_LONG).show();
     }
 
-    private void showError(@StringRes int errorMessage) {
-        Snackbar.make(findViewById(android.R.id.content), errorMessage, Snackbar.LENGTH_LONG).show();
+    @OnClick(R.id.button_google_signin)
+    public void onClick(View v) {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                        .build(),
+                RC_SIGN_IN);
 
     }
 
